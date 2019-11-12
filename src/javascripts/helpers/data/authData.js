3dc44969b3f692b2feaeff8e7f8442f5e41c5ee4
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-11-11 19:09:08 -0600

    board and the pins all delete

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -6,6 +6,7 @@ const authDiv = $('#auth');
 const board = $('#board');
 const home = $('#home');
 const logout = $('#navbar-button-logout');
+const back = $('#back');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
@@ -13,11 +14,13 @@ const checkLoginStatus = () => {
       board.removeClass('hide');
       home.removeClass('hide');
       logout.removeClass('hide');
+      back.removeClass('hide');
       authDiv.addClass('hide');
     } else {
       board.addClass('hide');
       home.addClass('hide');
       logout.addClass('hide');
+      back.addClass('hide');
       authDiv.removeClass('hide');
     }
   });
